424817deeb50bd1f0e1131480aaee2b0b68a8314
OSCAR ALBERTO MORA M�NDEZ <oscarmora18268@correo.itm.edu.co>
2023-09-01 19:49:27 -0500

    asignacion de variable

--- ejercicio8/Program.cs
+++ ejercicio8/Program.cs
@@ -1,3 +1,19 @@
 ﻿// Construir un programa que pida un número y luego diga si este número es par o no.
 
+// 1 clase y metodo
 
+public class Ejercicio8
+{
+    static void Main(string[] args)
+    {
+        // 2 declaracion de variables
+
+        double num;
+
+        // 3 pedir datos por pantalla
+
+        Console.WriteLine("Escriba un numero: ");
+        num = Convert.ToDouble(Console.ReadLine());
+
+    }
+}
